6f94043e1a1dc761317ddb3b663bfca50af1db42
dmydna <danimaydana9@gmail.com>
2026-06-18 17:42:36 -0300

    resolver bug al eliminar listing

--- src/main/java/com/techlab/store/repository/ListingRepository.java
+++ src/main/java/com/techlab/store/repository/ListingRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -32,7 +33,7 @@ public interface ListingRepository extends JpaRepository<Listing, Long>, JpaSpec
     @Query("SELECT l FROM Listing l")
     Page<Listing> findAllSoft(Pageable pageable);
 
-
+    @Modifying
     @Transactional
     @Query("DELETE FROM Review r WHERE r.listingId = :listingId AND r.status = :status")
     void deleteReviewByListingIdAndStatus(
