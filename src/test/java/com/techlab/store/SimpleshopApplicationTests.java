7572763e5c7a3019098877007fcd5c806f180280
dmydna <danimaydana9@gmail.com>
2026-09-19 22:23:47 -0300

    fix(test): arreglar compile tests

--- src/test/java/com/techlab/store/SimpleshopApplicationTests.java
+++ src/test/java/com/techlab/store/SimpleshopApplicationTests.java
@@ -2,12 +2,14 @@ package com.techlab.store;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // application-test.properties
 class SimpleshopApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+    void contextLoads() {
+    }
 
 }
